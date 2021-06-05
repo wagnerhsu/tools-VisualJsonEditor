ab74fa3ae126a46f241034075ca1738af310475c
Wagner Xu <wagnerhsu@hotmail.com>
2021-06-05 13:21:00 +0800

    Update files

--- src/VisualJsonEditor/Properties/AssemblyInfo.cs
+++ src/VisualJsonEditor/Properties/AssemblyInfo.cs
@@ -2,20 +2,9 @@
 using System.Runtime.InteropServices;
 using System.Windows;
 
-[assembly: AssemblyTitle("VisualJsonEditor")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Rico Suter")]
-[assembly: AssemblyProduct("VisualJsonEditor")]
-[assembly: AssemblyCopyright("Copyright © Rico Suter, 2020")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 [assembly: ComVisible(false)]
 
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None, 
     ResourceDictionaryLocation.SourceAssembly 
 )]
-
-[assembly: AssemblyVersion("2.10.*")]
---
+++ src/VisualJsonEditor/README.md
@@ -0,0 +1,5 @@
+﻿## 2021-06-05
+- Not support Costura.Fody
+## 2021-06-04
+- Update TargetFramework from 4.5 to 4.6.1
+- Update Assembly version and file version
